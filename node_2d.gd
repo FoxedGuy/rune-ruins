3d614e29f233da2c71cee111ae9f0e4e5542e52c
Miki <mik.jas@o2.pl>
2026-04-27 10:48:45 +0200

    Basic enemy class creation

---
+++ node_2d.gd
@@ -0,0 +1,17 @@
+class_name Enemy extends Node2D
+
+var enemy_name
+var enemy_type
+var health
+var x_pos
+var y_pos
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
