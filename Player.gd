3d44b7cd01ee762edff938922b07a23ab79ca491
FoxedGuy <gradziuk.damian@o2.pl>
2026-05-04 23:22:28 +0200

    made stand-alone Player scene, renamed files and little player script changes

--- Player.gd
+++ Player.gd
@@ -5,20 +5,28 @@ const JUMP_VELOCITY = -400.0
 @onready
 var sprite = $Sprite2D
 
-# TODO: should be changed along the way
-var health = 100
-var mana = 30
-var stamina = 50
-var is_attacking: bool = false
-var weapon 
+var health: int
+var mana: int
+var stamina: int
+var weapon
+
+# TODO: implement states
+var move_state
+var attack_state
+
+func _ready() -> void:
+	health = 100
+	mana = 50
+	stamina = 80
+
+signal health_changed(new_value)
 
 func attack() -> void:
-	is_attacking = true
 	stamina -= 10
-	is_attacking = false
 	
 func get_damage(damage: int) -> void:
 	health -= damage
+	health_changed.emit(health)
 
 func _input(event: InputEvent) -> void:
 	pass
@@ -29,12 +37,12 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("move_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left ", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
