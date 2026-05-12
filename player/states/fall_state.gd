4954700edcbae7066f034d5b3f20595adc21e8b5
FoxedGuy <gradziuk.damian@o2.pl>
2026-05-12 19:37:57 +0200

    updated states to be 'wall-aware'

--- player/states/fall_state.gd
+++ player/states/fall_state.gd
@@ -1,14 +1,26 @@
 extends PlayerState
 
+var is_from_wall
+var from_wall
+
 func _enter(previous_state_path: String, data := {}) -> void:
+	is_from_wall = data['is_from_wall'] if "is_from_wall" in data else false
+	from_wall = data['wall'] if is_from_wall else null
 	#player.animation_player.play("fall")
-	pass
 
 func update_physics(delta: float) -> void:
 	var input_direction_x := Input.get_axis("move_left", "move_right")
 	player.velocity.x = player.SPEED * input_direction_x
 	player.velocity += player.get_gravity() * delta
 	player.move_and_slide()
+	
+	if player.is_on_wall_only():
+		var wall = "right" if player.get_wall_side() < 0 else "left" 
+		
+		if not is_from_wall:
+			finished.emit(ONWALL, {"wall": wall})
+		elif wall != from_wall:
+			finished.emit(ONWALL, {"wall": wall})
 
 	if player.is_on_floor():
 		if is_equal_approx(input_direction_x, 0.0):
--- player/states/jump_state.gd
+++ player/states/jump_state.gd
@@ -1,7 +1,13 @@
 extends PlayerState
 
+var is_from_wall: bool
+var from_wall
+
 func _enter(previous_state_path: String, data := {}) -> void:
 	player.velocity.y = player.JUMP_VELOCITY
+	is_from_wall = data['is_from_wall'] if "is_from_wall" in data else false
+	from_wall = data['wall'] if is_from_wall else null
+
 	#player.animation_player.play("jump")
 
 func update_physics(delta: float) -> void:
@@ -9,6 +15,15 @@ func update_physics(delta: float) -> void:
 	player.velocity.x = player.SPEED * input_direction_x
 	player.velocity += player.get_gravity() * delta
 	player.move_and_slide()
-
+	
+	if player.is_on_wall_only():
+		var wall = "right" if player.get_wall_side() < 0 else "left" 
+		if not is_from_wall:
+			finished.emit(ONWALL, {"wall":wall})
+		elif wall != from_wall:
+			finished.emit(ONWALL, {"wall":wall})
 	if player.velocity.y >= 0:
-		finished.emit(FALLING)
+		if is_from_wall:
+			finished.emit(FALLING, {'is_from_wall': true, "wall": from_wall})
+		else: 
+			finished.emit(FALLING)
