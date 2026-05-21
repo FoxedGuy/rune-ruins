2fde0a624070bbf2262c1bdf8e6285cd6d685654
FoxedGuy <gradziuk.damian@o2.pl>
2026-05-21 20:11:38 +0200

    removed commented code

--- player/Player.gd
+++ player/Player.gd
@@ -44,22 +44,3 @@ func get_wall_side() -> PlayerEnums.WallSide:
 	
 func _process(delta:float) -> void:
 	label.text = state_machine.current_state.to_string()
-
-#func _physics_process(delta: float) -> void:
-	## Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-#
-	## Handle jump.
-	#if Input.is_action_just_pressed("move_jump") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-#
-	## Get the input direction and handle the movement/deceleration.
-	## As good practice, you should replace UI actions with custom gameplay actions.
-	#var direction := Input.get_axis("move_left", "move_right")
-	#if direction:
-		#velocity.x = direction * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-#
-	#move_and_slide()
